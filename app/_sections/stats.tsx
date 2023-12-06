82cebac808d7c2b2b0b27c49c3e9cb92e9d2a5d5
ibrahim <ibrahim.ozcan@finartz.com>
2023-12-06 15:41:33 +0300

    Update stats component styles

--- app/_sections/stats.tsx
+++ app/_sections/stats.tsx
@@ -1,9 +1,9 @@
 const Stat = ({value, label}: {value: string, label: string}) => {
     return <div>
-        <div className="text-[44px] font-[600]">
+        <div className="text-[24px] md:text-[44px] text-center font-[600]">
             {value}
         </div>
-        <div className="text-[#828282] text-[18px]">
+        <div className="text-center text-[#828282] text-[16px] md:text-[18px]">
             {label}
         </div>
     </div>
@@ -29,27 +29,27 @@ export const Stats = () => {
         </div>
 
         <div className="grid grid-cols-1 md5:grid-cols-2 mt-10 justify-between w-full items-center max-w-container mx-auto">
-            <div className="flex md5:hidden justify-center space-x-10">
-                <div className="text-[36px] font-[600]">
+            <div className="flex items-center md5:hidden justify-center space-x-2 md:space-x-10">
+                <div className="text-[18px] md:text-[36px] font-[600]">
                     Alınan Yatırımlar
                 </div>
-                <div className="text-[36px] font-[600]">
+                <div className="text-[18px] md:text-[36px] font-[600]">
                     1.5M $
                 </div>
                 <div className="text-[36px] font-[600]">
-                    <div className="w-[171px] h-[60px]">
-                        <img className="w-[171px] h-[60px] rounded-[13px] object-cover" alt="Rectangle" src="finberg_button.png" />
+                    <div className="w-[100px] md:w-[171px]">
+                        <img className="w-full rounded-[5px] md:rounded-[13px] object-cover" alt="Rectangle" src="finberg_button.png" />
                     </div>
                 </div>
             </div>
             <div className="flex items-center justify-center mt-10 md5:mb-0">
-                <div className="text-[32px] md5:text-[44px] font-[300]">
+                <div className="text-[24px] md:text-[32px] md5:text-[44px] font-[300]">
                     <span className="font-[600]">10+</span> Marka
                 </div>
                 <img src="Group 162748.png" className="h-[50px] md5:h-[111px] ml-5 md5:ml-auto md5:mr-10"/>
             </div>
             <div className="flex items-center justify-center md5:order-1 mt-10">
-                <div className="text-[18px] md5:text-[36px] font-[500]">
+                <div className="text-[18px] md:text-[34px] lg:text-[30px] 2xl:text-[36px] font-[500]">
                     Desteklenen Blockchain Ağı
                 </div>
                 <img src="avax.png" className="h-[34px] md5:h-[111px] ml-5 md5:ml-auto"/>
